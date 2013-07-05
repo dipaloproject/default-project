e89387bdb913326324363539d95e090f613a0763
tsepomaleka <malekatsepo@gmail.com>
2013-07-05 19:56:45 +0200

    Uploaded all project files

---
+++ DipaloSurvey/WebContent/javaScripts/default.js
@@ -0,0 +1,7 @@
+/**
+ * The default JavaScript file
+ */
+
+function initialise(){
+	
+}
---
+++ DipaloSurvey/WebContent/phpScripts/model/Constants.php
@@ -0,0 +1,14 @@
+<?php 
+	
+class Constants {
+
+	public static $HOST_NAME = "localhost";
+	public static $HOST_IP = "localhost";
+	public static $DB_NAME = "emerggpx_survey";
+	public static $PASSWORD = "";
+	public static $USERNAME = "emerggpx_root";
+
+	public static $WEB_BASE = "http://survey.emergesoftware.co.za";
+	
+}
+?>
